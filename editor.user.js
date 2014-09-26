c00bae2adcba89b63381005251d1e773f820b523
Cameron Bernhardt <cameronbernhardt@Camerons-MacBook-Pro.local>
2014-09-26 19:34:21 -0400

    Tweaked "edit" fix reason

--- editor.user.js
+++ editor.user.js
@@ -232,7 +232,7 @@ window.addEventListener("load", function(){
   		edit: {
   			expr: /[\*]*(edit|update):?[\*]*/gi,
   			replacement: "",
-  			reason: "Stack Exchange has an advanced revision history system: please don't include 'Edit' with edits, as the revision history makes the timing of your edits clear",
+  			reason: "Stack Exchange has an advanced revision history system: please don't include 'Edit' or 'Update' with edits, as the revision history makes the timing of your edits clear",
   		},
 
   		voting: {
