9857625bc29f2f70611801cda595562555961d7c
Tiny Giant <ajsa.ferguson@gmail.com>
2015-08-16 14:46:55 -0600

    Minor bugfix for the updater

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stack Exchange CV Request Generator
 // @namespace    http://your.homepage/
-// @version      1.2
+// @version      1.1
 // @description  This script generates formatted close vote requests and sends them to a specified chat room
 // @author       @TinyGiant
 // @match        http://*.stackoverflow.com/questions/*
@@ -37,8 +37,8 @@ with_jquery(function ($) {
     StackExchange.ready(function () {
         //// Self Updating Userscript, see https://gist.github.com/Benjol/874058
         // (the first line of this template _must_ be a comment!)
-        var VERSION = '1.2';
-        var URL = "https://github.com/SO-Close-Vote-Reviewers/UserScripts/raw/master/SECloseVoteRequestGenerator.user.js";
+        var VERSION = '1.1';
+        var URL = "https://rawgit.com/SO-Close-Vote-Reviewers/UserScripts/master/SECloseVoteRequestGenerator.user.js";
 
         if(window["SECloseVoteRequestGenerator_AutoUpdateCallback"]) {
             window["SECloseVoteRequestGenerator_AutoUpdateCallback"](VERSION);
