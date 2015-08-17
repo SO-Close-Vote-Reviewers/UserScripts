b36b43e3871ceb78cfe0640d134ed8c1910725da
Tiny Giant <ajsa.ferguson@gmail.com>
2015-08-17 10:41:20 -0600

    Version 1.4.2

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stack Exchange CV Request Generator
 // @namespace    https://github.com/SO-Close-Vote-Reviewers/
-// @version      1.4
+// @version      1.4.2
 // @description  This script generates formatted close vote requests and sends them to a specified chat room
 // @author       @TinyGiant
 // @match        http://*.stackoverflow.com/questions/*
