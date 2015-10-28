08b6c151075ffcf6e26a5fab65b6f9865c205c7b
David Bingham <mogsdad@users.noreply.github.com>
2015-10-27 23:15:36 -0400

    Merge pull request #57 from Tiny-Giant/dev-mogsdad
    Dev mogsdad

--- editor.user.js
+++ editor.user.js
@@ -414,8 +414,8 @@
                 replacement: "$1ersonal$2",
                 reason: "grammar and spelling"
             },
-            problem: { // https://regex101.com/r/yA8jM7/1
-                expr: /\b(p)(?:or|ro)b(?:le|el)m(s)?\b/gi,
+            problem: { // https://regex101.com/r/yA8jM7/3
+                expr: /\b(p)(?:or|ro)b(?:le|el|e)m(s)?\b/gi,
                 replacement: "$1roblem$2",
                 reason: "grammar and spelling"
             },
@@ -441,8 +441,13 @@
                 replacement: "",
                 reason: "noise reduction"
             },
-            hello: { // TODO: Update badsentences (new) to catch everything hello (old) did.
+            hello: { // TODO: Update badphrases (new) to catch everything hello and thanks (old) did.
                 expr: /(?:^|\s)(hi\s+guys|hi|hello|good\s(?:evening|morning|day|afternoon))(?:\.|!|\ )/gmi,
+                replacement: "",
+                reason: "noise reduction"
+            },
+            thanks: { // https://regex101.com/r/tV6uM4/2
+                expr: /[^\n.!?:]*\b(?:thanks|pl(?:ease|z|s)\s+h[ea]lp|cheers|regards|tanx|thx|thank\s+you|my\s+first\s+question|kind(?:ly)\shelp).*$/gmi,
                 replacement: "",
                 reason: "noise reduction"
             },
@@ -557,7 +562,7 @@
                 replacement: "$1xpect$2",
                 reason: "grammar and spelling"
             },
-            employe: {
+            employee: {
                 expr: /\b(e)mploye\b/gi,
                 replacement: "$1mployee",
                 reason: "grammar and spelling"
@@ -836,13 +841,13 @@
             },
             // From Peter Mortensen list (http://pvm-professionalengineering.blogspot.de/2011/04/word-list-for-editing-stack-exchange.html)
             ie: {  // http://english.stackexchange.com/questions/30106/can-i-start-a-sentence-with-i-e
-                expr: /\b(i|I)e\b/g,   // Careful here; IE is Internet Explorer
-                replacement: "$1.e.",
+                expr: /\b(i|I)e[.\s]+/g,   // Careful here; IE is Internet Explorer
+                replacement: "$1.e. ",
                 reason: "grammar and spelling"
             },
-            eg: {
-                expr: /\b(e)g\b/gi,
-                replacement: "$1.g.",
+            eg: { // https://regex101.com/r/qH2oT0/2
+                expr: /\b(e)g[.\s]+/gi,
+                replacement: "$1.g. ",
                 reason: "grammar and spelling"
             },
             unfortunately: {
@@ -975,8 +980,8 @@
                 replacement: "OK",
                 reason: "grammar and spelling"
             },
-            etc: {
-                expr: /\betc\b/g,
+            etc: {  // https://regex101.com/r/dE7cV1/3
+                expr: /\betc(?!\.)/g,
                 replacement: "etc.",
                 reason: "grammar and spelling"
             },
