dba7a56053d9bc1188ba881f63b8a31a8d95b06d
Tunaki <tunaki@somewhere.fr>
2016-06-08 17:17:56 +0200

    forgot to update version to 1.5.13

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Stack Exchange CV Request Generator
 // @namespace      https://github.com/SO-Close-Vote-Reviewers/
-// @version        1.5.12
+// @version        1.5.13
 // @description    This script generates formatted close vote requests and sends them to a specified chat room, fixes #65
 // @author         @TinyGiant
 // @contributor    @rene @Tunaki
