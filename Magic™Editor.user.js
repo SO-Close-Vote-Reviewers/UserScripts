dedcd49b954112c01e9676255e11cbe71294867d
Makyen <makyen@users.noreply.github.com>
2017-12-07 10:01:57 -0800

    Update version to 1.5.2.72

--- Magic™Editor.user.js
+++ Magic™Editor.user.js
@@ -10,7 +10,7 @@
 // @grant          none
 // @license        MIT
 // @namespace      http://github.com/SO-Close-Vote-Reviewers/UserScripts/Magic™Editor
-// @version        1.5.2.71
+// @version        1.5.2.72
 // @description    Fix common grammar/usage annoyances on Stack Exchange posts with a click
 //                 Forked from https://github.com/AstroCB/Stack-Exchange-Editor-Toolkit
 // @include        /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
