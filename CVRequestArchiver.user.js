46a2cfdce60ca676b827fb4f0a81041b480ff556
Makyen <wp.makyen@gmail.com>
2018-04-30 21:26:22 -0700

    Version 2.0.1.11.9 Add @updateURL and @downloadURL to facilitate merge to master.

--- CVRequestArchiver.user.js
+++ CVRequestArchiver.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         CV Request Archiver
 // @namespace    https://github.com/SO-Close-Vote-Reviewers/
-// @version      2.0.1.11.8
-// @description  (BETA) Scans the chat transcript and checks all cv+delete+undelete+reopen+dupe requests and SD+FireAlarm+Queen reports for status, then moves the completed or expired ones.
+// @version      2.0.1.11.9
+// @description  (BETA) Scans the chat transcript and checks all cv+delete+undelete+reopen+dupe requests and SD, FireAlarm, Queen, etc. reports for status, then moves the completed or expired ones.
 // @author       @TinyGiant @rene @Tunaki @Makyen
+// @updateURL    https://github.com/SO-Close-Vote-Reviewers/UserScripts/raw/master/CVRequestArchiver.user.js
+// @downloadURL  https://github.com/SO-Close-Vote-Reviewers/UserScripts/raw/master/CVRequestArchiver.user.js
 // @include      /https?:\/\/chat(\.meta)?\.stack(overflow|exchange).com\/(rooms|search|transcript)(\/|\?).*/
 // @grant        none
 // ==/UserScript==
