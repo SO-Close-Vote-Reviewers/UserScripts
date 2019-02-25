f04d6161571b34b10e397c958e6b220e170febe2
Makyen <wp.makyen@gmail.com>
2019-02-24 17:55:47 -0800

    CVRG: Don't use rawgit.com

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -13,7 +13,7 @@
 // @exclude        *://data.stackexchange.com/*
 // @require        https://code.jquery.com/jquery-2.1.4.min.js
 // @require        https://github.com/SO-Close-Vote-Reviewers/UserScripts/raw/master/gm4-polyfill.js
-// @connect        rawgit.com
+// @connect        github.com
 // @connect        raw.githubusercontent.com
 // @connect        chat.stackoverflow.com
 // @connect        chat.stackexchange.com
@@ -135,7 +135,7 @@ if(typeof StackExchange === "undefined")
     var reasons = currentSiteConfig.quickSubstitutions;
     var offTopicCloseReasons = currentSiteConfig.offTopicCloseReasons;
 
-    var URL = "https://rawgit.com/SO-Close-Vote-Reviewers/UserScripts/master/SECloseVoteRequestGenerator.user.js";
+    var URL = "https://github.com/SO-Close-Vote-Reviewers/UserScripts/raw/master/SECloseVoteRequestGenerator.user.js";
     var notifyint = 0;
     function notify(m,t) {
         var timeout;
@@ -177,7 +177,7 @@ if(typeof StackExchange === "undefined")
     function checkUpdates(force) {
         GM.xmlHttpRequest({
             method: 'GET',
-            url: 'https://rawgit.com/SO-Close-Vote-Reviewers/UserScripts/master/SECloseVoteRequestGenerator.version',
+            url: 'https://raw.githubusercontent.com/SO-Close-Vote-Reviewers/UserScripts/master/SECloseVoteRequestGenerator.version',
             onload: function(response) {
                 var VERSION = response.responseText.trim();
                 if(isVersionNewer(VERSION,GM.info.script.version)) {
