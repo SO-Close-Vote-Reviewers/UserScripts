06330fb258517cb5a0509659e6d20815f15a2438
Makyen <wp.makyen@gmail.com>
2019-03-03 15:50:55 -0800

    CVRG: Version 1.6.5

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Stack Exchange CV Request Generator
 // @namespace      https://github.com/SO-Close-Vote-Reviewers/
-// @version        1.6.4
+// @version        1.6.5
 // @description    This script generates formatted close vote requests and sends them to a specified chat room.
 // @author         @TinyGiant
 // @contributor    @rene @Tunaki @Makyen @paulroub
