a1d7452440441e2a23367431595bdcbc935097a1
Makyen <wp.makyen@gmail.com>
2021-10-29 16:53:25 -0700

    CVRG (alpha): Move executeInPage into a @require

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -13,6 +13,7 @@
 // @exclude        *://data.stackexchange.com/*
 // @require        https://code.jquery.com/jquery-3.5.0.min.js
 // @require        https://github.com/SO-Close-Vote-Reviewers/UserScripts/raw/master/gm4-polyfill.js
+// @require        https://cdn.jsdelivr.net/gh/makyen/extension-and-userscript-utilities@94cbac04cb446d35dd025974a7575b25b9e134ca/executeInPage.js
 // @connect        raw.githubusercontent.com
 // @connect        chat.stackoverflow.com
 // @connect        chat.stackexchange.com
@@ -31,10 +32,11 @@
 // @grant          GM.addValueChangeListener
 // ==/UserScript==
 /* jshint jquery:    true */
-/* globals unsafeWindow, StackExchange, Markdown, toStaticHTML, tagRendererRaw, GM_getValue, GM_setValue, GM_deleteValue, GM_addValueChangeListener, GM_openInTab, GM, $, jQuery */ // eslint-disable-line no-unused-vars, no-redeclare
+/* globals unsafeWindow, StackExchange, Markdown, toStaticHTML, tagRendererRaw, GM_getValue, GM_setValue, GM_deleteValue, GM_addValueChangeListener, GM_openInTab, GM, $, jQuery, makyenUtilities */ // eslint-disable-line no-unused-vars, no-redeclare
 
 (function() {
     'use strict';
+    const executeInPage = makyenUtilities.executeInPage;
 
     const parser = new DOMParser();
     //The RoomList is the list of chat rooms to which to send requests. It's effectively a class, but a one-off.
@@ -4147,100 +4149,6 @@
         cvplsRequestedAfterVote = detail.questionId;
         cvplsRequestedAfterVoteOptions = detail.options || {};
     }, true);
-
-    function executeInPage(functionToRunInPage, leaveInPage, id) {
-        //Execute a function in the page context.
-        // Any additional arguments passed to this function are passed into the page to the
-        // functionToRunInPage.
-        // Such arguments must be Object, Array, functions, RegExp,
-        // Date, and/or other primitives (Boolean, null, undefined,
-        // Number, String, but not Symbol).  Circular references are
-        // not supported. Prototypes are not copied.
-        // Using () => doesn't set arguments, so can't use it to define this function.
-        // This has to be done without jQuery, as jQuery creates the script
-        // within this context, not the page context, which results in
-        // permission denied to run the function.
-        function convertToText(args) {
-            //This uses the fact that the arguments are converted to text which is
-            //  interpreted within a <script>. That means we can create other types of
-            //  objects by recreating their normal JavaScript representation.
-            //  It's actually easier to do this without JSON.stringify() for the whole
-            //  Object/Array.
-            var asText = '';
-            var level = 0;
-
-            function lineSeparator(adj, isntLast) {
-                level += adj - ((typeof isntLast === 'undefined' || isntLast) ? 0 : 1);
-                asText += (isntLast ? ',' : '') + '\n' + (new Array((level * 2) + 1)).join('');
-            }
-
-            function recurseObject(obj) {
-                if (Array.isArray(obj)) {
-                    asText += '[';
-                    lineSeparator(1);
-                    obj.forEach(function(value, index, array) {
-                        recurseObject(value);
-                        lineSeparator(0, index !== array.length - 1);
-                    });
-                    asText += ']';
-                } else if (obj === null) {
-                    asText += 'null';
-                } else if (obj === void (0)) {
-                    //undefined
-                    asText += 'void(0)';
-                } else if (Number.isNaN(obj)) {
-                    //Special cases for Number
-                    //Not a Number (NaN)
-                    asText += 'Number.NaN';
-                } else if (obj === 1 / 0) {
-                    // +Infinity
-                    asText += '1/0';
-                } else if (obj === 1 / -0) {
-                    // -Infinity
-                    asText += '1/-0';
-                } else if (obj instanceof RegExp || typeof obj === 'function') {
-                    //function
-                    asText += obj.toString();
-                } else if (obj instanceof Date) {
-                    asText += 'new Date("' + obj.toJSON() + '")';
-                } else if (typeof obj === 'object') {
-                    asText += '{';
-                    lineSeparator(1);
-                    Object.keys(obj).forEach(function(prop, index, array) {
-                        asText += JSON.stringify(prop) + ': ';
-                        recurseObject(obj[prop]);
-                        lineSeparator(0, index !== array.length - 1);
-                    });
-                    asText += '}';
-                } else if (['boolean', 'number', 'string'].indexOf(typeof obj) > -1) {
-                    asText += JSON.stringify(obj);
-                } else {
-                    console.log('Didn\'t handle: typeof obj:', typeof obj, '::  obj:', obj);
-                }
-            }
-            recurseObject(args);
-            return asText;
-        }
-        var newScript = document.createElement('script');
-        if (typeof id === 'string' && id) {
-            newScript.id = id;
-        }
-        var args = [];
-        //Using .slice(), or other Array methods, on arguments prevents optimization.
-        for (var index = 3; index < arguments.length; index++) {
-            args.push(arguments[index]);
-        }
-        newScript.textContent = '(' + functionToRunInPage.toString() + ').apply(null,' +
-            convertToText(args) + ');';
-        (document.head || document.documentElement).appendChild(newScript);
-        if (!leaveInPage) {
-            //Synchronous scripts are executed immediately and can be immediately removed.
-            //Scripts with asynchronous functionality *may* need to remain in the page
-            //  until complete. Exactly what's needed depends on actual usage.
-            document.head.removeChild(newScript);
-        }
-        return newScript;
-    }
 
     function createMarkdownLinkWithText(text, url) {
         //Create a Markdown link with URL: [foo](//example.com/bar)
