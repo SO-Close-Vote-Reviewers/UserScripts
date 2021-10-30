f0344e6b053df5fa614266e3d28a887ac5811c05
Makyen <wp.makyen@gmail.com>
2021-10-29 19:56:40 -0700

    CVRG (alpha): Adjust @include to add search; @exclude main se.com

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -5,10 +5,11 @@
 // @description    ALPHA: This script generates formatted close-/delete-/reopen-/undelete-vote requests, spam/offensive flag requests, Smoke Detector reports, and approve-/reject-pls requests for suggested edits, then sends them to a specified chat room.
 // @author         @TinyGiant @Makyen
 // @contributor    @rene @Tunaki
-// @include        /^https?://(?:[^/.]+\.)*(?:stackexchange\.com|stackoverflow\.com|serverfault\.com|superuser\.com|askubuntu\.com|stackapps\.com|mathoverflow\.net)/(?:q(?:uestions)?\/\d+|review|tools|admin|users|$)/
+// @include        /^https?://(?:[^/.]+\.)*(?:stackexchange\.com|stackoverflow\.com|serverfault\.com|superuser\.com|askubuntu\.com|stackapps\.com|mathoverflow\.net)/(?:q(?:uestions)?\/\d+|review|tools|admin|users|search|\?|$)/
 // @exclude        *://chat.stackoverflow.com/*
 // @exclude        *://chat.stackexchange.com/*
 // @exclude        *://chat.*.stackexchange.com/*
+// @exclude        *://stackexchange.com/*
 // @exclude        *://api.*.stackexchange.com/*
 // @exclude        *://data.stackexchange.com/*
 // @require        https://code.jquery.com/jquery-3.5.0.min.js
