0ac4cac21f44e18b4c0dd8e6b8dca65050faef32
Makyen <wp.makyen@gmail.com>
2021-10-31 10:52:46 -0700

    CVRG: Version 2.0.0
    Move ALPHA version into release (Greasemonkey issues not resolved)
    Lots of features added vs. the most recent prior release version. Those are not listed here.
    Changes for this release vs. ALPHA Version 1.10.3.0:
      Basic dark mode support
      Fix things broken by prior SE changes:
        Functionality in Review queues
        Suggested edit popups
        NATO
      Adjust default options to show SD types on non-SOCVR sites and always send to Charcoal HQ
      Improve compatibility with some browsers (.attr() vs. .data())
      Get an uncorrupted version when MathJax is used
      Questions are allowed to be at -2 in SOCVR for a 20k del-pls.
      Answers are allowed to be at 0 in SOCVR for a 20k del-pls.
      Move executeInPage into a @require: DRY
      Add @downloadURL and @updateURL

--- SECloseVoteRequestGenerator.user.js
+++ SECloseVoteRequestGenerator.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Stack Exchange CV Request Generator
 // @namespace      https://github.com/SO-Close-Vote-Reviewers/
-// @version        1.10.3.0
-// @description    ALPHA: This script generates formatted close-/delete-/reopen-/undelete-vote requests, spam/offensive flag requests, Smoke Detector reports, and approve-/reject-pls requests for suggested edits, then sends them to a specified chat room.
+// @version        2.0.0
+// @description    This script generates formatted close-/delete-/reopen-/undelete-vote requests, spam/offensive flag requests, Smoke Detector reports, and approve-/reject-pls requests for suggested edits, then sends them to a specified chat room.
 // @author         @TinyGiant @Makyen
 // @contributor    @rene @Tunaki
 // @include        /^https?://(?:[^/.]+\.)*(?:stackexchange\.com|stackoverflow\.com|serverfault\.com|superuser\.com|askubuntu\.com|stackapps\.com|mathoverflow\.net)/(?:q(?:uestions)?\/\d+|review|tools|admin|users|search|\?|$)/
